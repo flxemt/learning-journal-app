4a77d174534b5c1a6cc38df4deb42f4e915cd147
flxemt <kudavidss@gmail.com>
2022-11-24 23:52:37 +0400

    fixed homepage

--- pages/homepage.js
+++ pages/homepage.js
@@ -1,5 +1,12 @@
 import view from '../utils/view';
 
+import image1 from '../assets/images/article-image-03.png';
+import image2 from '../assets/images/article-image-02.png';
+import image3 from '../assets/images/blog-image-05.png';
+import image4 from '../assets/images/blog-image-03.png';
+import image5 from '../assets/images/blog-image-01.png';
+import image6 from '../assets/images/article-image-01.png';
+
 function Homepage() {
   view.innerHTML = `
     <header>
@@ -12,37 +19,37 @@ function Homepage() {
     <main class="container">
       <section class="posts">
           <article class="post">
-              <img class="post-img" src="assets/images/article-image-03.png" alt="Laptop">
+              <img class="post-img" src="${image1}" alt="article image">
               <time class="post-date" datetime="2022-07-23">July 23, 2022</time>
               <h2 class="post-title">Blog one</h2>
               <p class="post-content">I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.</p>
           </article>
           <article class="post">
-              <img class="post-img" src="assets/images/article-image-02.png" alt="Laptop ">
+              <img class="post-img" src="${image2}" alt="Laptop ">
               <time class="post-date" datetime="2022-07-23">July 23, 2022</time>
               <h2 class="post-title">Blog two</h2>
               <p class="post-content">I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.</p>
           </article>
           <article class="post">
-              <img class="post-img" src="assets/images/blog-image-05.png" alt="Laptop">
+              <img class="post-img" src="${image3}" alt="Laptop">
               <time class="post-date" datetime="2022-07-23">July 23, 2022</time>
               <h2 class="post-title">Blog three</h2>
               <p class="post-content">I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.</p>
           </article>
           <article class="post">
-              <img class="post-img" src="assets/images/blog-image-03.png" alt="Laptop">
+              <img class="post-img" src="${image4}" alt="Laptop">
               <time class="post-date" datetime="2022-07-23">July 23, 2022</time>
               <h2 class="post-title">Blog four</h2>
               <p class="post-content">I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.</p>
           </article>
           <article class="post">
-              <img class="post-img" src="assets/images/blog-image-01.png" alt="Laptop">
+              <img class="post-img" src="${image5}" alt="Laptop">
               <time class="post-date" datetime="2022-07-23">July 23, 2022</time>
               <h2 class="post-title">Blog five</h2>
               <p class="post-content">I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.</p>
           </article>
           <article class="post">
-              <img class="post-img" src="assets/images/article-image-01.png" alt="Laptop">
+              <img class="post-img" src="${image6}" alt="Laptop">
               <time class="post-date" datetime="2022-07-23">July 23, 2022</time>
               <h2 class="post-title">Blog six</h2>
               <p class="post-content">I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.</p>
